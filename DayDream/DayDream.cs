ea95aeb974080c1a05bfe66037a68a928c4e92e6
Nick J. Connors <nicholasconnors@hotmail.ca>
2021-11-28 01:27:34 -0500

    Initial commit

---
+++ DayDream/DayDream.cs
@@ -0,0 +1,275 @@
+﻿using OWML.ModHelper;
+using OWML.Common;
+using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using OWML.Utils;
+using System.Reflection;
+
+namespace DayDream
+{
+    class DayDream : ModBehaviour
+    {
+        public static DayDream SharedInstance { get; private set; }
+
+        public static bool SeeSun { get; private set; }
+        public static float SunAngle { get; private set; }
+        public static bool SeeSolarSystem { get; private set; }
+        public static bool DreamAtAnyFire { get; private set; }
+        public static float SunIntensity { get; private set; }
+        public static float ShadowIntensity { get; private set; }
+
+        public Campfire CampfireSleptAt;
+        public RelativeLocationData CampfireRelativeLocation;
+
+        private float[] _previousCullLayerDistances = null;
+        private bool _sceneLoaded = false;
+        private Quaternion _baseRotation;
+        private Vector3 _rotationAxis;
+
+        private bool _initNextTick = false;
+        //private bool _teleportNextTick = false;
+
+        public List<OWCamera> Cameras { get; private set; }
+
+        public override void Configure(IModConfig config)
+        {
+            base.Configure(config);
+
+            var newSunIntensity = config.GetSettingsValue<float>("Sun intensity");
+            var flag = newSunIntensity != SunIntensity;
+            SunIntensity = newSunIntensity;
+
+            var newShadowIntensity = config.GetSettingsValue<float>("Shadow intensity");
+            flag |= newShadowIntensity != ShadowIntensity;
+            ShadowIntensity = newShadowIntensity;
+
+            var newSunAngle = config.GetSettingsValue<float>("Sun angle");
+            var flag1 = newSunAngle != SunAngle;
+            SunAngle = newSunAngle;
+
+            var newSeeSun = config.GetSettingsValue<bool>("See sun");
+            var flag2 = newSeeSun != SeeSun;
+            SeeSun = newSeeSun;
+
+            var newSeeSolarSystem = config.GetSettingsValue<bool>("See solar system");
+            flag2 |= (newSeeSolarSystem != SeeSolarSystem);
+            SeeSolarSystem = newSeeSolarSystem;
+
+            DreamAtAnyFire = config.GetSettingsValue<bool>("Dream at any fire");
+
+            if(_sceneLoaded)
+            {
+                WriteInfo("Settings changed");
+                if(flag && Locator.GetDreamWorldController().IsInDream())
+                {
+                    Locator.GetDreamWorldController().ApplySunOverrides(Locator.GetPlayerCamera(), default);
+                }
+                if (flag1)
+                {
+                    SetDreamWorldAngle();
+                }
+                if (flag2) ResetSolarSystemVisibility();
+            }
+        }
+
+        private void Start()
+        {
+            SharedInstance = this;
+
+            ModHelper.HarmonyHelper.AddPrefix<DreamWorldController>("ApplySunOverrides", typeof(Patches), nameof(Patches.ApplySunOverrides));
+            //ModHelper.HarmonyHelper.AddPrefix<DreamWorldController>("FixedUpdate", typeof(Patches), nameof(Patches.DreamWorldControllerFixedUpdate));
+            ModHelper.HarmonyHelper.AddPrefix<Campfire>("OnStopSleeping", typeof(Patches), nameof(Patches.OnStopSleeping));
+            ModHelper.HarmonyHelper.AddPrefix<PartyPathAction>("StartFollowPath", typeof(Patches), nameof(Patches.StartFollowPath));
+
+            GlobalMessenger.AddListener("EnterDreamWorld", OnEnterDreamWorld);
+            GlobalMessenger.AddListener("ExitDreamWorld", OnExitDreamWorld);
+
+            ModHelper.Console.WriteLine($"Mod {nameof(DayDream)} is loaded!", MessageType.Success);
+
+            SceneManager.sceneLoaded += OnSceneLoaded;
+        }
+
+        void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+        {
+            if (scene.name != "SolarSystem")
+            {
+                _sceneLoaded = false;
+                return;
+            }
+
+            _sceneLoaded = true;
+
+            // We want to save this for later to keep the rotation consistent.
+            var dreamWorld = Locator.GetAstroObject(AstroObject.Name.DreamWorld);
+            var sunPosition = Locator.GetAstroObject(AstroObject.Name.Sun).transform.position;
+            Vector3 toSun = dreamWorld.transform.position - sunPosition;
+            _baseRotation = Quaternion.LookRotation(Vector3.forward, -toSun);
+            _rotationAxis = dreamWorld.GetOWRigidbody().transform.TransformDirection(Vector3.right);
+
+            SetDreamWorldAngle();
+
+            // Compatibility with camera mods, they just have to add their cameras to this
+            Cameras = new List<OWCamera>();
+
+            _initNextTick = true;
+        }
+
+        private void SetDreamWorldAngle()
+        {
+            WriteInfo($"Setting sun angle to {SunAngle}");
+
+            var dreamWorld = Locator.GetAstroObject(AstroObject.Name.DreamWorld);
+            var rotation = _baseRotation * Quaternion.AngleAxis(SunAngle - 90, _rotationAxis);
+
+            if(Locator.GetDreamWorldController().IsInDream())
+            {
+                var oldParent = Locator.GetPlayerBody().transform.parent;
+                Locator.GetPlayerBody().transform.SetParent(dreamWorld.transform);
+                dreamWorld.GetOWRigidbody().SetRotation(rotation);
+                Locator.GetPlayerBody().transform.SetParent(oldParent);
+            }
+            else
+            {
+                dreamWorld.GetOWRigidbody().SetRotation(rotation);
+            }
+
+            float freqPerLoop = 0.1f; //Rotation per loop
+            dreamWorld.GetOWRigidbody().SetAngularVelocity(dreamWorld.transform.TransformDirection(Vector3.left) * freqPerLoop * 2f*Mathf.PI / 1320f);
+        }
+
+        private void ResetSolarSystemVisibility()
+        {
+            if (Locator.GetDreamWorldController().IsInDream())
+            {
+                //Reset camera settings
+                SetCameraSettings(false);
+                SetCameraSettings(true);
+            }
+        }
+
+        private void SetCameraSettings(bool entering)
+        {
+            foreach (var camera in Cameras)
+            {
+                if (entering)
+                {
+                    if (SeeSun)
+                    {
+                        // Make it so the sun layer is visible to the camera again
+                        camera.mainCamera.cullingMask |= 1 << LayerMask.NameToLayer("Sun");
+                    }
+                    else
+                    {
+                        camera.mainCamera.cullingMask &= ~(1 << LayerMask.NameToLayer("Sun"));
+                    }
+
+                    if (SeeSolarSystem)
+                    {
+                        camera.mainCamera.layerCullDistances = new float[32];
+                        camera.farClipPlane = Locator.GetDreamWorldController().GetValue<float>("_prevPlayerCameraFarPlaneDist");
+                    }
+                    else
+                    {
+                        camera.farClipPlane = 2000f;
+                    }
+
+                    if (SeeSun && !SeeSolarSystem)
+                    {
+                        // We reset the farClipPlane but put its value into layerCullDistances except for the sun layer.
+                        _previousCullLayerDistances = camera.mainCamera.layerCullDistances;
+                        var distances = new float[32];
+                        for (int i = 0; i < 32; i++)
+                        {
+                            distances[i] = (i == LayerMask.NameToLayer("Sun")) ? 0 : 2000f;
+                        }
+                        camera.mainCamera.layerCullDistances = distances;
+                        camera.mainCamera.farClipPlane = Locator.GetDreamWorldController().GetValue<float>("_prevPlayerCameraFarPlaneDist");
+                    }
+                }
+                else
+                {
+                    // Make sure the layerCullDistances are back to normal
+                    if (_previousCullLayerDistances != null)
+                    {
+                        camera.mainCamera.layerCullDistances = _previousCullLayerDistances;
+                        _previousCullLayerDistances = null;
+                    }
+                    camera.mainCamera.farClipPlane = Locator.GetDreamWorldController().GetValue<float>("_prevPlayerCameraFarPlaneDist");
+                }
+            }
+        }
+
+        private void OnEnterDreamWorld()
+        {
+            SetCameraSettings(true);
+        }
+
+        private void OnExitDreamWorld()
+        {
+            SetCameraSettings(false);
+
+            //if (CampfireSleptAt != null) _teleportNextTick = true;
+        }
+
+        private void Update()
+        {
+            if(_initNextTick)
+            {
+                _initNextTick = false;
+                Cameras.Add(Locator.GetPlayerCamera());
+            }
+
+            /*
+            if(_teleportNextTick)
+            {
+                // Teleport back to the campfire
+                var playerObj = Locator.GetPlayerBody().GetAttachedOWRigidbody();
+                var planetBody = CampfireSleptAt.GetAttachedOWRigidbody().GetReferenceFrame().GetOWRigidBody();
+
+                WriteInfo($"Teleporting to {planetBody.name}");
+
+                // Set new position
+                var relativePosition = CampfireSleptAt.transform.localPosition;//.TransformPoint(CampfireRelativeLocation.localPosition)on;
+                var position = planetBody.transform.TransformPoint(relativePosition);
+                playerObj.SetPosition(new Vector3(position.x, position.y, position.z));
+                playerObj.SetValue("_lastPosition", new Vector3(position.x, position.y, position.z));
+
+                var velocity = planetBody.GetPointTangentialVelocity(position) + planetBody.GetVelocity();
+                playerObj.SetVelocity(new Vector3(velocity.x, velocity.y, velocity.z));
+                playerObj.SetValue("_currentVelocity", new Vector3(velocity.x, velocity.y, velocity.z));
+                playerObj.SetValue("_lastVelocity", new Vector3(velocity.x, velocity.y, velocity.z));
+
+                var acceleration = planetBody.GetAcceleration();
+                playerObj.SetValue("_currentAccel", new Vector3(acceleration.x, acceleration.y, acceleration.z));
+                playerObj.SetValue("_lastAccel", new Vector3(acceleration.x, acceleration.y, acceleration.z));
+
+                var parentRotation = planetBody.GetRotation();
+                var rotation = parentRotation;
+                playerObj.SetRotation(new Quaternion(rotation.x, rotation.y, rotation.z, rotation.w));
+
+                var angularVelocity = planetBody.GetAngularVelocity();
+                playerObj.SetAngularVelocity(new Vector3(angularVelocity.x, angularVelocity.y, angularVelocity.z));
+                playerObj.SetValue("_currentAngularVelocity", new Vector3(angularVelocity.x, angularVelocity.y, angularVelocity.z));
+                playerObj.SetValue("_lastAngularVelocity", new Vector3(angularVelocity.x, angularVelocity.y, angularVelocity.z));
+                CampfireSleptAt = null;
+
+                if (!Physics.autoSyncTransforms)
+                {
+                    Physics.SyncTransforms();
+                }
+            }
+            */
+        }
+
+        public static void WriteInfo(string msg)
+        {
+            SharedInstance.ModHelper.Console.WriteLine(msg, MessageType.Info);
+        }
+    }
+}
---
+++ DayDream/Patches.cs
@@ -0,0 +1,180 @@
+﻿using OWML.Common;
+using OWML.ModHelper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using System.Reflection;
+
+namespace DayDream
+{
+    public class Patches
+    {
+        public static bool ApplySunOverrides(SunLightController.SunOverrideSettings __1, ref SunLightController.SunOverrideSettings __result, bool ____insideDream)
+		{ 
+			if(____insideDream)
+            {
+				__1.sunShadowStrength = DayDream.ShadowIntensity;
+				__1.sunIntensity = DayDream.SunIntensity;
+				__1.ambientIntensity = 0f; //Doesn't do anything it seems
+				__result = __1;
+				return false;
+			}
+
+            return true;
+        }
+
+		public static bool OnStopSleeping(Campfire __instance)
+		{
+			if (DayDream.DreamAtAnyFire)
+			{
+				RelativeLocationData relativeLocation = new RelativeLocationData(Locator.GetPlayerBody(), __instance.GetComponentInParent<OWRigidbody>(), __instance.transform);
+
+				DreamArrivalPoint.Location location = DreamArrivalPoint.Location.Zone1;
+				DreamArrivalPoint arrivalPoint = Locator.GetDreamArrivalPoint(location);
+				DreamCampfire dreamCampfire = Locator.GetDreamCampfire(location);
+				if (Locator.GetToolModeSwapper().GetItemCarryTool().GetHeldItemType() != ItemType.DreamLantern)
+					Locator.GetToolModeSwapper().GetItemCarryTool().PickUpItemInstantly(GameObject.FindObjectsOfType<DreamLanternItem>()[3]);
+				Locator.GetDreamWorldController().EnterDreamWorld(dreamCampfire, arrivalPoint, relativeLocation);
+
+				DayDream.SharedInstance.CampfireSleptAt = __instance;
+				DayDream.SharedInstance.CampfireRelativeLocation = relativeLocation;
+
+				return false;
+			}
+			return true;
+		}
+
+		/*
+		public static bool DreamWorldControllerFixedUpdate(DreamWorldController __instance, ref bool ____exitingDream, ref bool ____closingEyes, float ____exitDreamTime, 
+			GhostGrabController ____activeGhostGrabController, bool ____outsideLanternBounds, SimulationCamera ____simulationCamera, ProxyShadowLight ____proxyShadowLight,
+			ref bool ____insideDream, ref bool ____waitingToLightLantern, Sector ____dreamWorldSector, DreamLanternSocket ____playerLanternSocket, DreamLanternItem ____playerLantern,
+			DreamArrivalPoint ____dreamArrivalPoint, ref DreamCampfire ____dreamCampfire, bool ____suitUpOnWake, DreamWakeType ____wakeType, ref float ____prevPlayerCameraFarPlaneDist,
+			float ____cachedCamDegreesY, PlayerCameraEffectController ____playerCamEffectController, HeightmapAmbientLightRenderer ____playerCamAmbientLightRenderer)
+        {
+            if (DayDream.SharedInstance.CampfireSleptAt == null || !(____exitingDream && ____closingEyes && Time.time > ____exitDreamTime)) return true;
+
+			var EnterLanternBounds = typeof(DreamWorldController).GetMethod("EnterLanternBounds", BindingFlags.NonPublic | BindingFlags.Instance);
+			var OnDreamCampfireExtinguished = typeof(DreamWorldController).GetMethod("OnDreamCampfireExtinguished", BindingFlags.NonPublic | BindingFlags.Instance);
+
+			// So that we do the -= on the event thing (calling the function does that)
+			OnDreamCampfireExtinguished.Invoke(__instance, new object[] { });
+
+			Locator.GetPlayerCameraDetector().GetComponent<AudioDetector>().DeactivateAllVolumes(0f);
+			if (____activeGhostGrabController != null)
+			{
+				____activeGhostGrabController.ReleasePlayer();
+			}
+			if (____outsideLanternBounds)
+			{
+				EnterLanternBounds.Invoke(__instance, new object[] { });
+			}
+
+			____simulationCamera.OnExitDreamWorld();
+			SunLightController.UnregisterSunOverrider(__instance);
+			if (____proxyShadowLight != null)
+			{
+				____proxyShadowLight.enabled = true;
+			}
+			____exitingDream = false;
+			____closingEyes = false;
+			____insideDream = false;
+			____waitingToLightLantern = false;
+			if (Locator.GetToolModeSwapper().GetItemCarryTool().GetHeldItemType() != ItemType.DreamLantern)
+			{
+				Locator.GetToolModeSwapper().GetItemCarryTool().DropItemInstantly(____dreamWorldSector, __instance.transform);
+				if (____playerLanternSocket != null)
+				{
+					Locator.GetToolModeSwapper().GetItemCarryTool().UnsocketItemInstantly(____playerLanternSocket);
+				}
+				else
+				{
+					Locator.GetToolModeSwapper().GetItemCarryTool().PickUpItemInstantly(____playerLantern);
+				}
+			}
+
+			____playerLantern.OnExitDreamWorld();
+
+			var campfire = DayDream.SharedInstance.CampfireSleptAt;
+			var relativeSleepLocation = DayDream.SharedInstance.CampfireRelativeLocation;
+			var planetBody = campfire.GetAttachedOWRigidbody().GetReferenceFrame().GetAstroObject().GetOWRigidbody();
+			Locator.GetPlayerBody().MoveToRelativeLocation(relativeSleepLocation, planetBody, campfire.transform);
+			//Locator.GetPlayerBody().SetAngularVelocity(planetBody.GetAngularVelocity());
+
+			var forward = campfire.transform.position - Locator.GetPlayerBody().transform.position;
+			var up = Locator.GetPlayerBody().transform.position - planetBody.transform.position;
+			Locator.GetPlayerBody().SetRotation(Quaternion.LookRotation(forward, up));
+			GlobalMessenger.FireEvent("WarpPlayer");
+			if (!Physics.autoSyncTransforms)
+			{
+				Physics.SyncTransforms();
+			}
+
+			Locator.GetPlayerCameraController().SetDegreesY(____cachedCamDegreesY);
+			PlayerSectorDetector playerSectorDetector2 = Locator.GetPlayerSectorDetector();
+			playerSectorDetector2.RemoveFromAllSectors();
+			Sector sector2 = campfire.GetSector();
+			while (sector2 != null)
+			{
+				sector2.GetTriggerVolume().AddObjectToVolume(playerSectorDetector2.gameObject);
+				sector2 = sector2.GetParentSector();
+			}
+			if (Locator.GetRingWorldController() != null)
+			{
+				Locator.GetRingWorldController().OnExitDreamWorld();
+			}
+
+			//____dreamArrivalPoint.OnExitDreamWorld();
+			//____dreamCampfire.OnExitDreamWorld();
+			____dreamCampfire = null;
+			Locator.GetPlayerDetector().GetComponent<ForceApplier>().SkipNextFrame();
+			Locator.GetPlayerBody().GetComponent<AlignPlayerWithForce>().SkipNextFrame();
+
+			__instance.ExtinguishDreamRaft();
+			Locator.GetAudioMixer().UnmixDreamWorld();
+			Locator.GetAudioMixer().UnmixSleepAtCampfire(1f);
+			
+			if (____suitUpOnWake)
+			{
+				Locator.GetPlayerSuit().SuitUp(false, true, false);
+				Locator.GetPlayerSuit().PutOnHelmetAfterDelay(2f);
+			}
+
+			____playerCamEffectController.OpenEyes(0.5f, false);
+			ReticleController.Show();
+			Locator.GetPromptManager().SetPromptsVisible(true);
+			if (____playerCamAmbientLightRenderer != null)
+			{
+				____playerCamAmbientLightRenderer.enabled = false;
+			}
+
+			var _playerCamera = Locator.GetPlayerCamera();
+			_playerCamera.cullingMask |= 1 << LayerMask.NameToLayer("Sun");
+			_playerCamera.farClipPlane = ____prevPlayerCameraFarPlaneDist;
+			____prevPlayerCameraFarPlaneDist = 0f;
+			_playerCamera.mainCamera.backgroundColor = Color.black;
+			_playerCamera.postProcessingSettings.screenSpaceReflectionAvailable = false;
+			_playerCamera.postProcessingSettings.ambientOcclusionAvailable = true;
+			if (____wakeType != DreamWakeType.CampfireExtinguished && ____wakeType != DreamWakeType.Asphyxiation)
+			{
+				Locator.GetPlayerAudioController().OnExitDreamWorld(global::AudioType.PlayerGasp_Medium);
+			}
+
+			GlobalMessenger.FireEvent("ExitDreamWorld");
+
+			DayDream.SharedInstance.CampfireSleptAt = null;
+
+			return false;
+		}
+		*/
+
+		public static void StartFollowPath(PartyPathAction __instance, GhostController ____controller)
+        {
+			// The ghost rotation is hardcoded only for ghosts going to the house party, this is the first function I could find to hook onto and fix it
+			var up = Locator.GetAstroObject(AstroObject.Name.DreamWorld).GetOWRigidbody().transform.TransformDirection(Vector3.up).normalized;
+			____controller.transform.rotation = Quaternion.FromToRotation(____controller.transform.up, up);
+		}
+	}
+}
